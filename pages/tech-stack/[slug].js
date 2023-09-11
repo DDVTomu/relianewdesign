0ce6cbbd59e6a884c367b49ee66602dce638b46f
DDVTomu <phan031099@gmail.com>
2023-09-11 09:09:04 +0700

    Update [slug].js

--- pages/tech-stack/[slug].js
+++ pages/tech-stack/[slug].js
@@ -43,16 +43,16 @@ function convertIndustryData(item, isHasAuthor = false) {
 const IndustriesPage = ({ industry }) => {
   useEffect(() => {
     const container = document.querySelector(".list-operations");
-    const items = container.querySelectorAll(".list-operations__col");
+    const items = container?.querySelectorAll(".list-operations__col");
     const maxColumns = 3;
 
     function updateColumns() {
-      const itemCount = items.length;
+      const itemCount = items?.length;
       const columns = Math.min(itemCount, maxColumns);
       const itemWidth =
         columns > 0 ? `calc((100% - 20px * 2 ) / ${columns})` : `100%`;
 
-      items.forEach((item) => {
+      items?.forEach((item) => {
         item.style.width = itemWidth;
       });
     }
